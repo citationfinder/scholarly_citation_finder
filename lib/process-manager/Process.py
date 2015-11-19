40afd369485c6616e7e059bc90f898c385279eb4
Xennis <code@xennis.org>
2015-11-19 21:09:42 +0100

    Add process manager

---
+++ lib/process-manager/Process.py
@@ -0,0 +1,37 @@
+
+class Process():
+    
+    def __init__(self):
+        pass
+
+    def log(self, msg):
+        self.logger.info('{0} for run {1}: {2}'.format(self.__class__.__name__, self.run_name, msg))
+    
+    try:
+        if isinstance(self, HarvesterProcess):
+            return self.harvest(data, dep_results)
+        elif isinstance(self, ExtractorProcess): 
+            return self.extract(data, dep_results)
+    except RunnableError as r:
+         return r
+    except Exception as e:
+         e_info = sys.exc_info()
+         self.log(''.join(traceback.format_exception(*e_info)))
+         return  RunnableError('{0}: {1}'.format(e.__class__.__name__, e))    
+
+class HarvesterProcess(Process):
+    def harvest(self):
+        raise RunnableError('Override this method')
+    
+class ExtractorProcess(Process):
+    def extract(self):
+        raise RunnableError('Override this method')
+    
+
+class ProcessError(Exception):
+    
+    def __init__(self, msg):
+        self.msg = msg
+        
+    def __unicode__(self):
+        return "RunnableError: {0}".format(self.msg)
---
+++ lib/process-manager/ProcessRunner.py
@@ -0,0 +1,21 @@
+import logging
+
+class ProcessRunner:
+    
+    def __init__(self):
+        self.harvesterProcesses = []
+        self.extractorProcesses = []
+        self.processes = []
+        
+        #self.result_logger = logging.getLogger('result')
+        #self.runnable_logger = logging.getLogger('runnables')
+        #self.result_logger.setLevel(logging.INFO)
+        #self.runnable_logger.setLevel(logging.INFO)
+        
+    def add_process(self, process, output_results=True):
+        self.processes.append(processes)
+
+        if issubclass(process, HarvesterProcess):
+            self.harvesterProcesses.append(process)
+        if issubclass(process, ExtractorProcess):
+            self.harvesterProcesses.append(process)        
