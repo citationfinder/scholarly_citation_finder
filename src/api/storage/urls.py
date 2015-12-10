e79699fbef73a9c9ff7d63467d25d8153cceb8b1
Xennis <code@xennis.org>
2015-12-10 11:01:51 +0100

    compute coauthors

--- src/api/storage/urls.py
+++ src/api/storage/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^publications/$', views.publications_index, name='publications_index')
+    url(r'^publications/$', views.publications_index, name='publications_index'),
+    url(r'^coauthors/$', views.coauthors_index, name='coauthors_index')
 ]
--- src/api/storage/views.py
+++ src/api/storage/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 import config
 from .Parser import Parser
+from core.models import Author, Publication
 
 
 def publications_index(request):
@@ -15,3 +16,12 @@ def publications_index(request):
         return HttpResponse('Finish')
 
     return HttpResponse('Nothing to do. Usage ?filelist=&lt;sample.xml&gt;')
+
+
+def coauthors_index(request):
+    authors = Author.objects.all()
+    for author in authors:
+        for author_publication in author.publication_set.all():
+            for coauthor in author_publication.authors.exclude(last_name=author.last_name):
+                author.coauthors.add(coauthor)
+    return HttpResponse('Finish')
