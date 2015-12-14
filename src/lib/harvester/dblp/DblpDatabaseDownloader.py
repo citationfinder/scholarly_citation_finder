567ed466ca9d6acda4fbb0613ff8f3175f697027
Xennis <code@xennis.org>
2015-12-14 10:46:30 +0100

    Fix dblp downloader

--- src/lib/harvester/dblp/DblpDatabaseDownloader.py
+++ src/lib/harvester/dblp/DblpDatabaseDownloader.py
@@ -2,8 +2,7 @@ import os.path
 import filecmp
 import logging
 
-import config
-from ...utils import create_dir, download_file, unzip_file
+from lib.utils import create_dir, download_file, unzip_file
 
 logger = logging.getLogger()
 
@@ -11,25 +10,27 @@ logger = logging.getLogger()
 class DblpDatabaseDownloader:
     
     DBLP_BASE_URL = 'http://dblp.uni-trier.de/xml/'
-    DBLP_DIR = os.path.join(config.DOWNLOAD_DIR, 'dblp')
     DBLP_FILE_XML_GZ = 'dblp.xml.gz'
     DBLP_FILE_MD5 = 'dblp.xml.gz.md5'
     
-    def __init__(self):
-        create_dir(self.DBLP_DIR)
-        
+    def __init__(self, download_dir):
+        self.download_dir = download_dir
+        create_dir(download_dir)
+    
+    def download(self):
         if self.is_new_data_avaible():
-            self.download_database()
+            return self._download_database()
         else:
             logger.debug('No new data is avaiable')
-            
+            return False
+     
     """
     Checks if new data is avaiable:
     Downloads the MD5-file of the XML and compares it with the local stored MD5-file
     """
     def is_new_data_avaible(self):
         logger.debug('Check is new data avaiable')
-        old_md5_file = self.DBLP_DIR + self.DBLP_FILE_MD5
+        old_md5_file = os.path.join(self.download_dir, self.DBLP_FILE_MD5)
         if os.path.isfile(old_md5_file):
             new_md5_file = download_file(self.DBLP_BASE_URL + self.DBLP_FILE_MD5, '')
             if filecmp.cmp(new_md5_file, old_md5_file, shallow=False):
@@ -39,10 +40,13 @@ class DblpDatabaseDownloader:
     """
     Downloads the XML- and the DTD-file from DBLP
     """
-    def download_database(self):
+    def _download_database(self):
         logger.debug('Download (and unzip) data')
-        download_file(self.DBLP_BASE_URL + self.DBLP_FILE_MD5, self.DBLP_DIR)
-        download_file(self.DBLP_BASE_URL + 'dblp.dtd', self.DBLP_DIR)
-        download_file(self.DBLP_BASE_URL + self.DBLP_FILE_XML_GZ, self.DBLP_DIR)
-        # Override old md5 file
-        #shutil.move(self.DBLP_FILE_MD5, self.DBLP_DIR + self.DBLP_FILE_MD5)
+        download_file(self.DBLP_BASE_URL + self.DBLP_FILE_MD5, self.download_dir)
+        download_file(self.DBLP_BASE_URL + 'dblp.dtd', self.download_dir)
+        xml_database_file = download_file(self.DBLP_BASE_URL + self.DBLP_FILE_XML_GZ, self.download_dir)
+        
+        if xml_database_file:
+            return unzip_file(xml_database_file)
+        else:
+            return False
--- src/lib/harvester/dblp/__main__.py
+++ src/lib/harvester/dblp/__main__.py
@@ -1,8 +1,14 @@
 import os.path
 
 import config
+from .DblpDatabaseDownloader import DblpDatabaseDownloader
 from .DblpHarvester import DblpHarvester
 
+DBLP_DIR = os.path.join(config.DOWNLOAD_DIR, 'harvester', 'dblp')
+
+
 if __name__ == '__main__':
+    #downloader = DblpDatabaseDownloader(DBLP_DIR)
+    #downloader.download()
     harvester = DblpHarvester()
-    harvester.harvest(os.path.join(config.DOWNLOAD_DIR, 'dblp', 'dblp.xml'))
+    harvester.harvest(os.path.join(DBLP_DIR, 'dblp.xml'))
