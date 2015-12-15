efd75ea1dc1406deade337bdbb839987da6ad304
Xennis <code@xennis.org>
2015-12-15 11:42:29 +0100

    switch to sickle for oai

--- src/lib/harvester/citeseerx/CiteseerxHarvester.py
+++ src/lib/harvester/citeseerx/CiteseerxHarvester.py
@@ -1,9 +1,8 @@
 import string
 
 from ..common.Harvester import Harvester
-from oaipmh.client import Client
-from oaipmh.metadata import MetadataRegistry, oai_dc_reader
-
+from sickle import Sickle
+from sickle.oaiexceptions import BadArgument
 
 class CiteseerxHarvester(Harvester):
     
@@ -21,53 +20,59 @@ class CiteseerxHarvester(Harvester):
     def __init__(self):
         super(CiteseerxHarvester, self).__init__('citeseerx')
     
-    def harvest(self):
-        registry = MetadataRegistry()
-        registry.registerReader('oai_dc', oai_dc_reader)
-        client = Client(self.OAI_PHM_URL, registry)
-        #harvestStart = datetime.strptime("2014-06-17T23:59:59Z", "%Y-%m-%dT%H:%M:%SZ")
-        for record in client.listRecords(metadataPrefix='oai_dc'):
-            header = record[0]
-            metadata = record[1]
+    def harvest(self, _from='2015-12-07'):
+        list_records_options = {
+            'metadataPrefix': 'oai_dc'
+        }
+        if _from:
+            list_records_options['from'] = _from
+        
+        client = Sickle(self.OAI_PHM_URL)
+        records = client.ListRecords(**list_records_options)
+        try:
+            for record in records:
+                metadata = record.metadata
+                
+                result_entry = {
+                    'urls': []
+                }
+        
+                if 'creator' in metadata:
+                    result_entry['authors'] = metadata['creator']
+                if 'date' in metadata:
+                    date = metadata['date'][-1]
+                    '''
+                    <dc:date>2009-04-19</dc:date>
+                    <dc:date>2007-11-19</dc:date>
+                    <dc:date>1998</dc:date>
+                    '''
+                    if len(date) == 4:
+                        result_entry['date'] = date
+                if 'source' in metadata:
+                    url = metadata['source'][0]
+                    if 'format' in metadata:
+                        url = {
+                            'value': url,
+                            'type': metadata['format'][0]
+                        }
+                    result_entry['urls'].append(url)
+                for field in self.FIELD_MAPPING:
+                    if metadata[field]:
+                        result_entry[self.FIELD_MAPPING[field]] = metadata[field][0]
+                
+                # <identifier>oai:CiteSeerX.psu:10.1.1.1.1519</identifier>
+                result_entry['citeseerx_id'] = string.replace(record.header.identifier, 'oai:CiteSeerX.psu:', '')
+                result_entry['urls'].append({
+                    'value': 'http://citeseerx.ist.psu.edu/viewdoc/download?doi={}&amp;rep=rep1&amp;type=pdf'.format(result_entry['citeseerx_id']),
+                    'type': 'application/pdf'
+                })
+                
+                self.open_split_file()
+                self.parse_publication2(result_entry)
+                
+                if self.check_stop_harvest():
+                    break
+        except (BadArgument) as e: # handle bug: https://github.com/mloesch/sickle/issues/8
+            self.logger.warn(str(e))
             
-            result_entry = {
-                'urls': []
-            }
-    
-            if metadata['creator']:
-                result_entry['authors'] = metadata['creator']
-            if metadata['date']:
-                date = metadata['date'][-1]
-                '''
-                <dc:date>2009-04-19</dc:date>
-                <dc:date>2007-11-19</dc:date>
-                <dc:date>1998</dc:date>
-                '''
-                if len(date) == 4:
-                    result_entry['date'] = date
-            if metadata['source']:
-                url = metadata['source'][0]
-                if metadata['format']:
-                    url = {
-                        'value': url,
-                        'type': metadata['format'][0]
-                    }
-                result_entry['urls'].append(url)
-            for field in self.FIELD_MAPPING:
-                if metadata[field]:
-                    result_entry[self.FIELD_MAPPING[field]] = metadata[field][0]
-            
-            # <identifier>oai:CiteSeerX.psu:10.1.1.1.1519</identifier>
-            result_entry['citeseerx_id'] = string.replace(header.identifier(), 'oai:CiteSeerX.psu:', '')
-            result_entry['urls'].append({
-                'value': 'http://citeseerx.ist.psu.edu/viewdoc/download?doi={}&amp;rep=rep1&amp;type=pdf'.format(result_entry['citeseerx_id']),
-                'type': 'application/pdf'
-            })
-            
-            self.open_split_file()
-            self.parse_publication2(result_entry)
-            
-            if self.check_stop_harvest():
-                break
-
         self.stop_harvest()
