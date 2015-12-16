bbb75e9eff1f0e7baf2f8443daa01b8ff349d8f5
Xennis <code@xennis.org>
2015-12-16 01:05:16 +0100

    update settings

--- search_for_citations/settings/development.py
+++ search_for_citations/settings/development.py
@@ -112,7 +112,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
 
 # Logging
