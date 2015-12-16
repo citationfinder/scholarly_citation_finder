283448bcbdc4934973c254f9f0fdb5ce1075e61e
Xennis <code@xennis.org>
2015-12-16 01:08:21 +0100

    update settings

--- search_for_citations/settings/development.py
+++ search_for_citations/settings/development.py
@@ -112,7 +112,15 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-#STATIC_URL = '/static/'
+STATIC_ROOT = os.getenv('STATIC_ROOT',
+                        os.path.join(BASE_DIR, 'collected_static'))
+
+STATIC_URL = '/static/'
+
+# Additional locations of static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 # Logging
