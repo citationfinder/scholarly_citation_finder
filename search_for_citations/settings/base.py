240d040ba4654f9e6fda6448a94bd0bbc3b36b77
Xennis <code@xennis.org>
2016-01-07 18:17:31 +0100

    candy

--- search_for_citations/settings/base.py
+++ search_for_citations/settings/base.py
@@ -89,8 +89,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = os.getenv('STATIC_ROOT',
-                        os.path.join(BASE_DIR, 'collected_static'))
+STATIC_ROOT = os.getenv('STATIC_ROOT', os.path.join(BASE_DIR, 'collected_static'))
 
 STATIC_URL = '/static/'
 
