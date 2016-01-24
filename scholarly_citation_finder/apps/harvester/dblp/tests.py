3e731b4790a7631439ffdbf6bd5c88b9d516ef80
Xennis <code@xennis.org>
2016-01-24 19:51:17 +0100

    add dblp harvester

---
+++ scholarly_citation_finder/apps/harvester/dblp/tests.py
@@ -0,0 +1,28 @@
+import os.path
+from django.test import TestCase
+
+from search_for_citations import config
+from .DblpHarvester import DblpHarvester
+
+
+class DblpHarvesterTest(TestCase):
+
+    TEST_DIR = os.path.join(config.TEST_DIR, 'harvester', 'dblp')
+    TEST_XML = os.path.join(TEST_DIR, 'dblp_tiny.xml')
+
+    def setUp(self):
+        self.harvester = DblpHarvester()
+
+    def test_harvest_io_error(self):
+        self.assertRaises(IOError, self.harvester.harvest, 'non-existing-file.xml')
+
+    def test_harvest_file_creation(self):
+        self.harvester.harvest(self.TEST_XML)
+        first = os.path.isfile(os.path.join(config.DOWNLOAD_DIR, 'harvester', 'dblp', 'publication-0.xml'))
+        self.assertEqual(first, True)
+
+    """
+    def test_harvest(self):
+        self.harvester.harvest(self.TEST_DIR + 'dblp_tiny.xml')
+        #self.assertEqual(first, 4, 'Test insert 4 publications from DBLP XML file')
+    """
