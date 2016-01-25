9cb2f11cf0734a8dbaeedc02ce8063cc4a25b0af
Xennis <code@xennis.org>
2016-01-25 23:50:50 +0100

    update django static path

--- scholarly_citation_finder/settings/base.py
+++ scholarly_citation_finder/settings/base.py
@@ -83,7 +83,8 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_ROOT = os.getenv('STATIC_ROOT', os.path.join(BASE_DIR, 'collected_static'))
+STATIC_ROOT = os.getenv('STATIC_ROOT',
+                        os.path.join(BASE_DIR, 'static'))
 STATIC_URL = '/static/'
 
 # Additional locations of static files.
