ea93ed3d58ce2bf1dd001e2f4ad550878221b7be
Xennis <code@xennis.org>
2016-01-27 23:31:28 +0100

    update crawler package

--- scholarly_citation_finder/api/crawler/PdfFinder.py
+++ scholarly_citation_finder/api/crawler/PdfFinder.py
@@ -4,24 +4,62 @@ import re
 from urlparse import urlparse
 from requests.exceptions import ConnectionError
 import logging
+
 logger = logging.getLogger(__name__)
 
 
 class PdfFinder:
+    '''
+    Module to find a or multiple PDFs files on a HTML page.
+    '''
 
     MIMETYPE_PDF = 'application/pdf'
     MIMETYPE_HTML = 'text/html'
 
-    def get_pdf(self, url):
-        links = self.get_links(url)
-        if links:
-            for link in self.get_links(url):
+    def find_pdf(self, url):
+        '''
+        Looks for PDFs files on the provided website.
+        
+        :param url: HTML page
+        '''
+        hyperrefs = self.find_hyperrefs(url)
+        if hyperrefs:
+            for link in hyperrefs:
                 if link.endswith('.pdf') or link.endswith('/pdf'):
                     return link
-            return links[-1]
+            return hyperrefs[-1]
         return False
 
-    def get_links_from_html(self, html, url):
+    def find_hyperrefs(self, url):
+        '''
+        Looks for hyperrefs to PDFs file on the provided website.
+        
+        :param url: HTML page
+        '''
+        try:
+            r = requests.get(url)
+            r_type = r.headers.get('content-type')
+
+            # the URL is already a PDF
+            if r_type in self.MIMETYPE_PDF:
+                return url
+            # the URL is a HTML page
+            elif r_type in self.MIMETYPE_HTML:
+                return self.find_hyperrefs_from_html(r.text, url)
+            else:
+                logger.debug('Unknown header type: {}'.format(r_type))
+        except(ConnectionError) as e:
+            logger.info(e, exc_info=True)
+
+        return False
+
+    def find_hyperrefs_from_html(self, html, url):
+        '''
+        Looks for hyperrefs to PDFs file in the provided HTML string.
+        
+        :param html: HTML string
+        :param url: URL the HTML was downloaded from. It is used to convert relative URLs to absolute ones
+        '''
         result = []
         parsed_uri = urlparse(url)
         url_domain = '{uri.scheme}://{uri.netloc}'.format(uri=parsed_uri)
@@ -39,19 +77,3 @@ class PdfFinder:
                 href = url + href
             result.append(href)
         return result
-
-    def get_links(self, url):
-        try:
-            r = requests.get(url)
-            r_type = r.headers.get('content-type')
-
-            if r_type in self.MIMETYPE_PDF:
-                return url
-            elif r_type in self.MIMETYPE_HTML:
-                return self.get_links_from_html(r.text, url)
-            else:
-                logger.debug('Unknown header type: {}'.format(r_type))
-        except(ConnectionError) as e:
-            logger.debug(str(e))
-
-        return False
--- scholarly_citation_finder/api/crawler/tests.py
+++ scholarly_citation_finder/api/crawler/tests.py
@@ -3,19 +3,19 @@ from django.test import TestCase
 from .PdfFinder import PdfFinder
 
 
-class Test(TestCase):
+class PdfFinderTest(TestCase):
 
     def setUp(self):
-        self.crawler = PdfFinder()
+        self.pdffinder = PdfFinder()
 
     def test_get_links(self):
         #first = self.crawler.get_links('http://www.sciencedirect.com/science/article/pii/S1477842410000394')
         pass
 
-    def test_get_pdf(self):
-        first = self.crawler.get_pdf('http://link.springer.com/book/10.1007%2F978-3-642-19357-6')
+    def test_find_pdf(self):
+        first = self.pdffinder.find_pdf('http://link.springer.com/book/10.1007%2F978-3-642-19357-6')
         self.assertEqual(first, 'http://link.springer.com/content/pdf/10.1007%2F978-3-642-19357-6.pdf')
-        first = self.crawler.get_pdf('http://www.mdpi.com/2224-2708/2/2/172')
+        first = self.pdffinder.find_pdf('http://www.mdpi.com/2224-2708/2/2/172')
         self.assertEqual(first, 'http://www.mdpi.com/2224-2708/2/2/172/pdf')
-        first = self.crawler.get_pdf('http://www.computer.org/csdl/proceedings/dcoss/2011/0512/00/05982219-abs.html')
+        first = self.pdffinder.find_pdf('http://www.computer.org/csdl/proceedings/dcoss/2011/0512/00/05982219-abs.html')
         self.assertEqual(first, 'http://www.computer.org/csdl/proceedings/dcoss/2011/0512/00/05982219.pdf')
