2c294635270336ed80165e988f7948971faef591
Xennis <code@xennis.org>
2016-01-28 00:02:27 +0100

    update harvester class

--- scholarly_citation_finder/api/harvester/Harvester.py
+++ scholarly_citation_finder/api/harvester/Harvester.py
@@ -1,10 +1,6 @@
-import os.path
 import getopt
 import sys
-import time
 
-from search_for_citations import config
-from .. import utils
 from ..Parser import Parser
 
 
@@ -35,21 +31,10 @@ def get_arguments(argv):
 class Harvester(Parser):
     
     def __init__(self, name):
-        super(Harvester, self).__init__('{}'.format(name))
-        utils.create_dir(os.path.join(config.DOWNLOAD_DIR, 'harvester', name))
-        self.split_publications = 10000
-        self.start_time = int(time.time())
-        self.logger.info('start at {}'.format(self.start_time))
-        
-    def open_split_file(self):
-        if self.count_publications % self.split_publications == 0:
-            file_num = self.count_publications / self.split_publications
-            if file_num > 0:
-                self.close_output_file()
-            self.open_output_file(os.path.join(config.DOWNLOAD_DIR, 'harvester', self.name, '{}-{}.xml'.format(self.start_time, file_num)))
+        super(Harvester, self).__init__(name)
+        self.logger.info('start {} harvester'.format(name))
         
     def stop_harvest(self):
-        self.close_output_file()
         self.logger.info('stop')
     
     def check_stop_harvest(self):
