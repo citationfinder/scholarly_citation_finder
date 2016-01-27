b3f4367954f574d7d930bc67197cdcd69fe73075
Xennis <code@xennis.org>
2016-01-27 17:09:38 +0100

    merge 3/4: merge sfc with scf

--- search_for_citations/apps/core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Author, Publication, PublicationReference, PublicationUrl
-
-
-admin.site.register(Author)
-admin.site.register(Publication)
-admin.site.register(PublicationReference)
-admin.site.register(PublicationUrl)
