ca99fe141e231a4a516644b11edede58b4cccff6
Xennis <code@xennis.org>
2016-02-01 19:34:51 +0100

    add author strategy

--- scholarly_citation_finder/api/citation/CitationFinder.py
+++ scholarly_citation_finder/api/citation/CitationFinder.py
@@ -9,9 +9,20 @@ class CitationFinder(Process):
         self.conn = self.get_database_connection(database_name)
         self.cursor = self.conn.cursor()
         self.logger.info('init {} ------------------'.format(name))
-        self.output = open(os.path.join(self.download_dir, '{}.csv'.format(name)))
+        
+    def _array2sqllist(self, list):
+        #return ','.join([str(i) for i in list])
+        return '('+str(list)[1:-1]+')'
+    
+    def _sqllist2array(self, sqllist):
+        #result = '('+','.join([item[0] for item in sqllist])+')'
+        return [item[0] for item in sqllist]
+    
+    def _open_output_file(self, name):
+        self.output = open(os.path.join(self.download_dir, '{}.csv'.format(name)), 'w+')
         
     def set_publications(self, publication_ids):
+        self.logger.info('set {} publications as search set'.format(len(publication_ids)))
         self.publication_ids = publication_ids
         
     def set_author(self, name):
@@ -20,14 +31,16 @@ class CitationFinder(Process):
         :param name: Name of the author
         '''
         
-        self.cursor.execute("SELECT id FROM core_author WHERE name = %s LIMIT 1", (name,))
+        self.cursor.execute("SELECT id FROM core_author WHERE name LIKE %s LIMIT 1", (name,))
         result = self.cursor.fetchone()
         
         if result:
             id = result[0]
-            self.logger.info('search author "{}", found: {}'.format(name, id))
+            self._open_output_file(id)
+            self.logger.info('search author "{}", found author id: {}'.format(name, id))
             self.cursor.execute("SELECT publication_id FROM core_publicationauthoraffilation WHERE author_id = %s", (id,))
-            self.set_publications(self.cursor.fetchall())
+            self.set_publications(self._sqllist2array(self.cursor.fetchall()))
+            return True
         else:
             self.logger.info('search author "{}", found nothing'.format(name))
         return False          
