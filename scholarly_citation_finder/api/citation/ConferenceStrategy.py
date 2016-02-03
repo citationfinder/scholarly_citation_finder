7adc10c1fccc64fed21c08e97b65ce7a189a8f74
Xennis <code@xennis.org>
2016-02-03 18:00:51 +0100

    add field of study strategy

--- scholarly_citation_finder/api/citation/ConferenceStrategy.py
+++ scholarly_citation_finder/api/citation/ConferenceStrategy.py
@@ -19,7 +19,7 @@ class ConferenceStrategy(CitationFinder):
         conferences = self.__find_conferences()
         self.logger.info('found {} conferences in search set'.format(len(conferences)))
         for conference in conferences:
-            conference_publications = self.__find_journal_publications(conference.id)            
+            conference_publications = self.__find_conference_publications(conference.id)            
             conference_publications_citing = citing_papers.filter(publication__in=conference_publications)
             self.logger.info('conference "{}": found {} publications, {} citations'.format(conference, len(conference_publications), len(conference_publications_citing)))
             
@@ -32,5 +32,5 @@ class ConferenceStrategy(CitationFinder):
     def __find_conferences(self):
         return Conference.objects.using(self.database_name).filter(publication__in=self.publication_search_set).distinct()
     
-    def __find_journal_publications(self, conference_id):
+    def __find_conference_publications(self, conference_id):
         return Publication.objects.using(self.database_name).filter(conference_id=conference_id)
---
+++ scholarly_citation_finder/api/citation/FieldofstudyStrategy.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from CitationFinder import CitationFinder
+from scholarly_citation_finder.apps.core.models import Publication, PublicationReference, FieldOfStudy
+
+class FieldofstudyStrategy(CitationFinder):
+
+
+    def __init__(self):
+        super(FieldofstudyStrategy, self).__init__(name='fieldofstudy_strategy')
+        
+    def run(self, publication_limit=None, time_limit=None):
+        CitationFinder.run(self, publication_limit=publication_limit, time_limit=time_limit)
+
+        citing_papers = PublicationReference.objects.using(self.database_name).filter(reference__in=self.publication_search_set)
+        
+    
+        fieldofstudies = self.__find_fieldofstudies()
+        self.logger.info('found {} field of studies in search set'.format(len(fieldofstudies)))
+        for fieldofstudy in fieldofstudies:
+            fieldofstudy_publications = self.__find_fieldofstudy_publications(fieldofstudy.id)            
+            fieldofstudy_publications_citing = citing_papers.filter(publication__in=fieldofstudy_publications)
+            self.logger.info('field of study "{}": found {} publications, {} citations'.format(fieldofstudy, len(fieldofstudy_publications), len(fieldofstudy_publications_citing)))
+            
+            for publicationReference in fieldofstudy_publications_citing:
+                if publicationReference not in self.publicationreferences_result_set:
+                    self.publicationreferences_result_set.append(publicationReference)
+            self.output.write_values(len(fieldofstudy_publications), len(self.publicationreferences_result_set))
+        self.run_done()
+        
+    def __find_fieldofstudies(self):
+        return FieldOfStudy.objects.using(self.database_name).filter(publicationkeyword__publication__in=self.publication_search_set).distinct()
+    
+    def __find_fieldofstudy_publications(self, fieldofstudy_id):
+        return Publication.objects.using(self.database_name).filter(publicationkeyword__fieldofstudy_id=fieldofstudy_id)
