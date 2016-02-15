29eec9a300f12263d6f5c10c2ab00e32b9cdbdb9
Xennis <code@xennis.org>
2016-02-15 11:17:38 +0100

    add from param to dblp harvester

--- scholarly_citation_finder/api/harvester/dblp/DblpHarvester.py
+++ scholarly_citation_finder/api/harvester/dblp/DblpHarvester.py
@@ -42,11 +42,12 @@ class DblpHarvester(Harvester):
     def __init__(self, **kwargs):
         super(DblpHarvester, self).__init__('dblp', **kwargs)
     
-    def harvest(self, filename=None, limit=None):
+    def harvest(self, filename=None, limit=None, _from=None):
         '''
         
-        :param filename:
-        :param limit:
+        :param filename: DBLP XML file name
+        :param limit: Number of maximum publications to parse
+        :param _from: Last stored (!) DBLP key, e.g. 'journals/jlp/Winter08'
         :return: Number of parsed publications
         '''
         if filename is None:
@@ -60,18 +61,19 @@ class DblpHarvester(Harvester):
         if os.path.isfile(filename):
             self.start_harevest()
             context = etree.iterparse(filename, load_dtd=True, html=True)
-            num_publications = self._fast_iter(context)
+            num_publications = self._fast_iter(context, _from=_from)
             self.stop_harvest()
             return num_publications
         else:
             raise IOError('File {} not found'.format(filename))
 
-    def _fast_iter(self, context):
+    def _fast_iter(self, context, _from=None):
         '''
         @see: https://github.com/Ajeo/dblp-to-csv/blob/master/parser.py
         @param context : iterparsed (chunk of xml) data
         
         :param context:
+        :param _from: Last stored (!) DBLP key, e.g. 'journals/jlp/Winter08'
         :return: The number of parsed publications
         '''
         cite_writer = CsvFileWriter()
@@ -84,59 +86,63 @@ class DblpHarvester(Harvester):
         urls = []
         citations = []
         
-        #read chunk line by line
-        #we focus author and title
         for _, elem in context:
             if elem.tag == 'html' or elem.tag == 'body':
                 continue
-            # title
-            elif elem.tag == 'title' and elem.text:
-                title = elem.text
-                if title.endswith('.'):
-                    title = title[:-1]
-                publication[self.FIELD_MAPPING[elem.tag]] = title
-            # author and editor
-            elif elem.tag in ('author', 'editor') and elem.text:
-                field = self.FIELD_MAPPING['author']
-                authors.append(elem.text)
-            # pages
-            elif elem.tag == 'pages' and elem.text:
-                pages = elem.text.split('-')
-                publication['pages_from'] = pages[0]
-                if len(pages) > 1:
-                    publication['pages_to'] = pages[1]
-            # ee
-            elif elem.tag == 'ee' and elem.text:
-                field = self.FIELD_MAPPING['ee']
-                urls.append(elem.text)
-
-                if 'http://dx.doi.org/' in elem.text:
-                    publication['doi'] = elem.text.replace('http://dx.doi.org/', '')
-            # crossref
-            elif elem.tag == 'crossref' and elem.text:
-                conference_short_name = elem.text.split('/')[1] # 'conf/naa/2008' -> 'naa'
-            elif elem.tag == 'cite' and elem.text:
-                if elem.text != '...':
-                    citations.append(elem.text)
-            # journal_name
-            elif elem.tag == 'journal' and elem.text:
-                journal_name = elem.text
-            # other
-            elif elem.tag in self.FIELD_MAPPING and elem.text:
-                publication[self.FIELD_MAPPING[elem.tag]] = elem.text
-            elif elem.tag in ('inproceedings', 'incollection', 'www'):
-                publication.clear()
-                conference_short_name = None
-                journal_name = None
-                authors = []
-                urls = []
-                citations = []           
+            # If from is not None, skip all these steps
+            elif _from is None:
+                # title
+                if elem.tag == 'title' and elem.text:
+                    title = elem.text
+                    if title.endswith('.'):
+                        title = title[:-1]
+                    publication['title'] = title
+                # author and editor
+                elif elem.tag in ('author', 'editor') and elem.text:
+                    authors.append(elem.text)
+                # pages
+                elif elem.tag == 'pages' and elem.text:
+                    pages = elem.text.split('-')
+                    publication['pages_from'] = pages[0]
+                    if len(pages) > 1:
+                        publication['pages_to'] = pages[1]
+                # ee
+                elif elem.tag == 'ee' and elem.text:
+                    urls.append(elem.text)
+                    # doi
+                    if 'http://dx.doi.org/' in elem.text:
+                        publication['doi'] = elem.text.replace('http://dx.doi.org/', '')
+                # crossref
+                elif elem.tag == 'crossref' and elem.text:
+                    conference_short_name = elem.text.split('/')[1] # 'conf/naa/2008' -> 'naa'
+                elif elem.tag == 'cite' and elem.text:
+                    if elem.text != '...':
+                        citations.append(elem.text)
+                # journal_name
+                elif elem.tag == 'journal' and elem.text:
+                    journal_name = elem.text
+                # other
+                elif elem.tag in self.FIELD_MAPPING and elem.text:
+                    publication[self.FIELD_MAPPING[elem.tag]] = elem.text
+                #
+                elif elem.tag in ('inproceedings', 'incollection', 'www'):
+                    publication.clear()
+                    conference_short_name = None
+                    journal_name = None
+                    authors = []
+                    urls = []
+                    citations = []           
             # collaboration
-            elif elem.tag in self.COLLABORATIONS:
+            if elem.tag in self.COLLABORATIONS:
+                key = elem.get('key')
+                if _from is not None:
+                    # Stop skipping, if the key was found
+                    if _from == key:
+                        _from = None
+                    continue
                 
                 publication['type'] = elem.tag
-                publication['source'] = 'dbpl:'+elem.get('key')
-
+                publication['source'] = 'dbpl:'+key
 
                 # store and clear entry afterwards
                 publication_id = self.parse(publication,
@@ -170,4 +176,4 @@ class DblpHarvester(Harvester):
 
 #if __name__ == '__main__':
 #    harvester = DblpHarvester()
-#    harvester.harvest(limit=100000)
+#    harvester.harvest(_from='journals/jlp/Winter08')
