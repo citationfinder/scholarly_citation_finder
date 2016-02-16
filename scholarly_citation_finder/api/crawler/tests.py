562ebdd9f60032730f5ee51427a7ded584d865f4
Xennis <code@xennis.org>
2016-02-16 13:58:43 +0100

    improve duckduckgo

--- scholarly_citation_finder/api/crawler/tests.py
+++ scholarly_citation_finder/api/crawler/tests.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
 
 from .PdfFinder import PdfFinder
+from Duckduckgo import Duckduckgo
 
-
+"""
 class PdfFinderTest(TestCase):
 
     def setUp(self):
@@ -19,3 +20,24 @@ class PdfFinderTest(TestCase):
         self.assertEqual(first, 'http://www.mdpi.com/2224-2708/2/2/172/pdf')
         first = self.pdffinder.find_pdf('http://www.computer.org/csdl/proceedings/dcoss/2011/0512/00/05982219-abs.html')
         self.assertEqual(first, 'http://www.computer.org/csdl/proceedings/dcoss/2011/0512/00/05982219.pdf')
+"""
+
+class DuckduckgoTest(TestCase):
+    
+    TEST_KEYWORDS = 'kernel completion for learning consensus support vector machines in bandwidth limited sensor networks'
+    
+    def setUp(self):
+        self.searchengine = Duckduckgo()
+        
+    def test_query_sucess(self):
+        first, _ = self.searchengine.query_publication(self.TEST_KEYWORDS)
+        self.assertEqual(first, 'http://www-ai.cs.uni-dortmund.de/PublicPublicationFiles/lee_poelitz_2014a.pdf')
+        
+        first, _ = self.searchengine.query_publication('advances in cloud and ubiquitous computing')
+        self.assertEqual(first, 'http://www.ronpub.com/publications/OJCC_2015v2i2n01e_Groppe.pdf')
+        
+    def test_query_failed(self):
+        first, _ = self.searchengine.query_publication('Automated composition and execution of hardware-accelerated operator graphs')
+        self.assertEqual(first, False)
+        
+        
