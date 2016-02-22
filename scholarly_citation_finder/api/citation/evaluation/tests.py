3342475af3afd46af4c94306af3166c72a3ba73f
Xennis <code@xennis.org>
2016-02-22 15:42:21 +0100

    add min pub args

--- scholarly_citation_finder/api/citation/evaluation/tests.py
+++ scholarly_citation_finder/api/citation/evaluation/tests.py
@@ -4,13 +4,13 @@ from django.test import TestCase
 import os.path
 
 from scholarly_citation_finder import config
-from scholarly_citation_finder.api.citation.evaluation.EvaluationAuthorSet import EvaluationAuthorSet
+from scholarly_citation_finder.api.citation.evaluation.Evaluation import Evaluation
 
 """
 class EvaluationAuthorSetTest(TestCase):
 
     def setUp(self):
-        self.author_set = EvaluationAuthorSet('run_a')
+        self.author_set = Evaluation('run_a')
         
     def test_run(self):
         self.author_set.run(2)
