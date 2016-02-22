9615d076180673f087c7feb9386371c337146c91
Xennis <code@xennis.org>
2016-02-22 23:34:46 +0100

    add celery; add secret key by env

--- scholarly_citation_finder/settings/base.py
+++ scholarly_citation_finder/settings/base.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e5zjfb$nbtq(u(go5q9etj^t@mhl2kr&w_np%3_zq+g4rx1n!l'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'e5zjfb$nbtq(u(go5q9etj^t@mhl2kr&w_np%3_zq+g4rx1n!l')
 
 
 # Application definition
