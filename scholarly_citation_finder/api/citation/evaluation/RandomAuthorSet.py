18ac4a4a1d27ef00098b585d07480c8fadd4dd6b
Xennis <code@xennis.org>
2016-02-24 05:13:31 +0100

    addd random author test

--- scholarly_citation_finder/api/citation/evaluation/RandomAuthorSet.py
+++ scholarly_citation_finder/api/citation/evaluation/RandomAuthorSet.py
@@ -6,15 +6,14 @@ logger = logging.getLogger(__name__)
 
 class RandomAuthorSet:
     
-    random_authors = []
-    
     def __init__(self, database='mag'):
         self.database = database
+        self.random_authors = []
         
     def get(self):
         return self.random_authors
 
-    def create(self, setsize, num_min_publications=0):        
+    def create(self, setsize, num_min_publications=0):
         # init
         random_nums = []; # stores random numbers to prevent duplicates
         num_stored_authors = Author.objects.using(self.database).count()
@@ -26,7 +25,7 @@ class RandomAuthorSet:
             if random_num in random_nums:
                 continue
 
-            author = authors[random_num:random_num+1][0]
+            author = authors[random_num]
             author_publications = Publication.objects.using(self.database).raw('SELECT publication_id AS id FROM core_publicationauthoraffilation WHERE author_id = %s', [author.id])
             author_num_publications = len(list(author_publications))
             if author_num_publications >= num_min_publications:
@@ -36,12 +35,12 @@ class RandomAuthorSet:
                                             'num_citations': author_num_citations})
             
                 random_nums.append(random_num)        
-        
+
     def load(self, filename):
         pass
         
     def store(self, filename):
         with open(filename, 'w+') as output_file:
-            dict_writer = csv.DictWriter(output_file, self.random_authors[0].keys)
+            dict_writer = csv.DictWriter(output_file, self.random_authors[0].keys())
             dict_writer.writeheader()
             dict_writer.writerows(self.random_authors)
--- scholarly_citation_finder/api/citation/evaluation/tests.py
+++ scholarly_citation_finder/api/citation/evaluation/tests.py
@@ -3,17 +3,41 @@
 from django.test import TestCase
 import os.path
 
-from scholarly_citation_finder import config
 from RandomAuthorSet import RandomAuthorSet
+from scholarly_citation_finder.apps.core.models import PublicationAuthorAffilation
 
-"""
-class EvaluationAuthorSetTest(TestCase):
+
+class RandomAuthorSetTest(TestCase):
+    fixtures = ['sample_data']
+
+    TMP_FILE = 'tmp'
 
     def setUp(self):
-        self.author_set = RandomAuthorSet('run_a')
+        self.author_set = RandomAuthorSet(database='default')
         
-    def test_run(self):
-        self.author_set.run(2)
-        first = os.path.isfile(os.path.join(self.author_set.download_dir, 'authors.csv'))
-        self.assertTrue(first)
-"""
+    def tearDown(self):
+        if os.path.isfile(self.TMP_FILE):
+            os.remove(self.TMP_FILE)
+     
+    def test_create_setsize(self):
+        setsize = 3
+        
+        self.author_set.random_authors = []
+        self.author_set.create(setsize)     
+        first = len(self.author_set.get())
+        self.assertEqual(first, setsize)
+
+    def test_create_num_min_publications(self):
+        num_min_publications = 4
+        
+        self.author_set.random_authors = []
+        self.author_set.create(1, num_min_publications)
+        author_id = self.author_set.get()[0]['author_id']
+        first = PublicationAuthorAffilation.objects.using(self.author_set.database).filter(author_id=author_id).count()
+        self.assertGreaterEqual(first, num_min_publications)
+ 
+    def test_store(self):
+        self.author_set.random_authors = []
+        self.author_set.create(3)
+        self.author_set.store(self.TMP_FILE)
+        self.assertTrue(os.path.isfile(self.TMP_FILE))
