234033861baba736103ad24847f020ecee3ec1ba
Xennis <code@xennis.org>
2016-02-26 22:29:14 +0100

    add publin callback

--- scholarly_citation_finder/api/citation/mag/tasks.py
+++ scholarly_citation_finder/api/citation/mag/tasks.py
@@ -2,17 +2,19 @@ from __future__ import absolute_import
 from celery import shared_task
 import os.path
 import logging
+import requests
 from django.core.exceptions import ObjectDoesNotExist
 
 from scholarly_citation_finder import config
 from scholarly_citation_finder.lib.file import create_dir
 from scholarly_citation_finder.api.citation.CitationFinder import CitationFinder
 
+
 logger = logging.getLogger(__name__)
 
 
 @shared_task
-def citations(author_name, author_id):
+def citations(author_name, author_id, publin_callback_url=None):
     try:
         citationfinder = CitationFinder()
         author_id, length_publication_set = citationfinder.publication_set.set_by_author(name=author_name, id=author_id)
@@ -22,6 +24,21 @@ def citations(author_name, author_id):
     
         # -> convert result
         output_path = create_dir(os.path.join(config.DOWNLOAD_DIR, 'mag'))
-        return citationfinder.store(filename=os.path.join(output_path, '{}.json'.format(author_id)))
+        output_filename = citationfinder.store(filename=os.path.join(output_path, '{}.json'.format(author_id)))
+        if publin_callback_url:
+            __publin_callback(publin_callback_url, output_filename)
+        else:
+            return output_filename
     except(ObjectDoesNotExist) as e:
         raise e
+
+
+def __publin_callback(callback_url, output_filename):
+    with open(output_filename, 'r') as output_file:
+        r = requests.post(callback_url,
+                          params={'p': 'submit', 'm': 'bulkimportapi'},
+                          data={'input': output_file.read()})
+    if r.status_code == 202:
+        return r.text
+    else:
+        raise Exception('Expected 202 as response, but get {}'.format(r.status_code))
--- scholarly_citation_finder/api/citation/mag/views.py
+++ scholarly_citation_finder/api/citation/mag/views.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import os.path
 from django.http import HttpResponse
 from djcelery.models import TaskMeta
 from django.core.exceptions import ObjectDoesNotExist
@@ -12,8 +13,9 @@ from scholarly_citation_finder.apps.tasks.models import Task
 def index(request):
     author_name = request.GET.get('author_name', None)
     author_id = request.GET.get('author_id', None)
+    publin_callback_url = request.GET.get('publin_callback_url', None)
     if author_name or author_id:
-        asyncresult = tasks.citations.delay(author_name=author_name, author_id=int(author_id))
+        asyncresult = tasks.citations.delay(author_name=author_name, author_id=int(author_id), publin_callback_url=publin_callback_url)
         task = Task.objects.create(type=Task.TYPE_CITATION_MAG, taskmeta_id=asyncresult.id)
         return JsonResponse(task.as_dict())
     else:
@@ -24,9 +26,11 @@ def task_detail(request, id):
     try:
         task = Task.objects.get(pk=id)
         result, tastmeta = task.result()
-        if result:
+        if os.path.isfile(result):
             with open(result) as result_file:
                 return HttpResponse(result_file, content_type='application/json')
+        elif result:
+            return HttpResponse(str(result))
         else:
             return JsonResponse(tastmeta)
     except(ObjectDoesNotExist):
