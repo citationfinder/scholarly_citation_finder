467ccb604927c2bd91cf41f866937010b3fd7902
Xennis <code@xennis.org>
2016-03-09 00:22:07 +0100

    client: add support for multiple extractors

--- public/client/src/js/scf/controller/state/pdf_tools/ExtractPdfController.js
+++ public/client/src/js/scf/controller/state/pdf_tools/ExtractPdfController.js
@@ -1,7 +1,17 @@
 function ExtractPdfController($scope, Restangular) {
-	$scope.extractFile = function(url) {
+	
+	$scope.extractors = [
+		{id: 'grobid', label: 'Grobid'},
+		{id: 'citeseer', label: 'Citeseer'}
+	];
+	
+	$scope.taskExtractor = $scope.extractors[0].id;
+
+	$scope.extractFile = function(extractor, url) {
+		console.warn(url);
+		console.warn(extractor);
 		$scope.showProgress = true;
-		Restangular.all('extractor').getList({url: url}).then(function(items) {
+		Restangular.all('extractor/' + extractor + '/').getList({url: url}).then(function(items) {
 			$scope.extractFileResult = items;
 			$scope.showProgress = false;
 		}, function(data) {
