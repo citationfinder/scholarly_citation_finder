6fc84fe1af9739e5c88f3c487bb6ce6527ecbba7
Xennis <code@xennis.org>
2016-03-09 20:18:23 +0100

    * improve tei parser to detect author, title and conference name correct * add tei test and new test data

--- scholarly_citation_finder/api/extractor/grobid/TeiParser.py
+++ scholarly_citation_finder/api/extractor/grobid/TeiParser.py
@@ -1,5 +1,8 @@
+import logging
 from lxml import etree
 from io import BytesIO
+
+logger = logging.getLogger(__name__)
 
 
 class TeiParser:
@@ -16,25 +19,39 @@ class TeiParser:
         
         publication = {}
         journal_name = None
+        conference_instance_name = None
         authors = []
-        tmp_author_name = ''
+        tmp_author_name = None
         
         for _, elem in context:
             if elem.tag == 'forename':
-                tmp_author_name += elem.text
+                if tmp_author_name is None:
+                    tmp_author_name = elem.text
+                else:
+                    tmp_author_name += ' '+elem.text
             elif elem.tag == 'surname':
-                tmp_author_name = elem.text + ',' + tmp_author_name
+                if tmp_author_name is None:
+                    tmp_author_name = elem.text
+                else:
+                    tmp_author_name = elem.text + ',' + tmp_author_name
             elif elem.tag == 'author':
                 authors.append(tmp_author_name)
-                tmp_author_name = ''
+                tmp_author_name = None
             elif elem.tag == 'title' and 'level' in elem.attrib:
+                level = elem.attrib.get('level')
                 # @see http://www.tei-c.org/release/doc/tei-p5-doc/de/html/ref-title.html
-                if elem.attrib['level'] == 'a':
+                if elem.attrib.get('type') == 'main':
+                    #if elem.attrib.get('level') in ('a', 'm'):
                     publication['title'] = elem.text
-                elif elem.attrib['level'] == 'j':
+                elif level == 'j':
                     journal_name = elem.text
-                elif elem.attrib['level'] == 'b':
+                elif level == 'b':
                     publication['booktitle'] = elem.text
+                elif level == 'm':
+                    conference_instance_name = elem.text
+                else:
+                    logger.warn('Unkown title with level {}: {}'.format(level, elem.text))
+                # else: 's', 'u'
             elif elem.tag == 'biblscope' and 'unit' in elem.attrib:
                 if elem.attrib['unit'] == 'volume':
                     publication['volume'] = elem.text
@@ -62,10 +79,14 @@ class TeiParser:
                                     },
                                    'publication': publication.copy(),
                                    'journal_name': journal_name,
+                                   'conference_instance_name': conference_instance_name,
                                    'authors': authors })
+                else:
+                    logger.info('skip, no title found')
                 
                 publication.clear()
                 journal_name = None
+                conference_instance_name = None
                 authors = []
                 tmp_author_name = ''
 
--- scholarly_citation_finder/api/extractor/grobid/tests.py
+++ scholarly_citation_finder/api/extractor/grobid/tests.py
@@ -19,16 +19,31 @@ class GrobidExtractorTest(TestCase):
         first = self.extractor.extract_file(self.TEST_PDF)
         self.assertEqual(first, True)
 
-
 class TeiParserTest(TestCase):
     
     TMP_OUTPUT_FILE = os.path.join(config.TEST_DIR, 'tmp.xml')
-    SAMPLE_TEI_CITATION_FILE = os.path.join(config.TEST_DIR, 'extractor', 'gropit', 'references.xml')
+    SAMPLE_TEI_CITATION_FILE = os.path.join(config.TEST_DIR, 'extractor', 'grobit', 'references.xml')
     
     def setUp(self):
         self.parser = TeiParser()
         
     def test_parse(self):
-        self.parser.output.open(self.TMP_OUTPUT_FILE)
-        self.parser.parse(self.SAMPLE_TEI_CITATION_FILE)
-        self.parser.output.close()
+        result = self.parser.parse(open(self.SAMPLE_TEI_CITATION_FILE).read())
+        
+        self.assertEqual(len(result), 2)
+        
+        # check title
+        self.assertEqual(result[0]['publication']['title'], 'Example title')
+        self.assertEqual(result[1]['publication']['title'], 'Another pub')
+        # check author
+        self.assertEqual(result[0]['authors'][0], 'Cluster,A B')
+        self.assertEqual(result[0]['authors'][1], 'Cabrerizo,F')
+        # check publication infos
+        self.assertEqual(result[0]['publication']['volume'], '3')
+        self.assertEqual(result[0]['publication']['pages_from'], '161')
+        self.assertEqual(result[0]['publication']['pages_to'], '168')
+        self.assertEqual(result[0]['publication']['year'], '2009')
+        
+        # confernce and journal name        
+        self.assertEqual(result[0]['conference_instance_name'], 'Example conference')
+        self.assertEqual(result[1]['journal_name'], 'Example journal')
