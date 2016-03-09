75aaf14fd90099a72ce03b25bd7026075f4bf9f8
Xennis <code@xennis.org>
2016-03-09 17:59:08 +0100

    declare utf-8 encoding in base files

--- scholarly_citation_finder/wsgi.py
+++ scholarly_citation_finder/wsgi.py
@@ -1,12 +1,5 @@
-"""
-WSGI config for scholarly_citation_finder project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
-"""
-
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import os
 
 from django.core.wsgi import get_wsgi_application
