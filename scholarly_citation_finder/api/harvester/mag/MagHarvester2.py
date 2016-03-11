94eeb6d90fc5e411b8c2010b9f9131ab65016d41
Xennis <code@xennis.org>
2016-03-11 15:04:43 +0100

    update new mag harvester

--- scholarly_citation_finder/api/harvester/mag/MagHarvester2.py
+++ scholarly_citation_finder/api/harvester/mag/MagHarvester2.py
@@ -1,18 +1,26 @@
+import os.path
 import csv
 import logging
+from django.db.utils import IntegrityError
 
 from scholarly_citation_finder.apps.core.models import FieldOfStudyHierarchy
-from django.db.utils import IntegrityError
+from .MagNormalize import MagNormalize
+
 
 logger = logging.getLogger(__name__)
 
 
 class MagHarvester2:
 
-    def __init__(self, path):
+    def __init__(self, path, database='mag'):
         self.path = path
+        self.database = database
         
-    def run(self, filename, callback):
+    def run(self, name, callback):
+        if name not in MagNormalize.FILES:
+            raise Exception('unknown name')
+        
+        filename = os.path.join(self.path, MagNormalize.FILES[name])
         with open(filename, 'rb') as csvfile:
             reader = csv.reader(csvfile, delimiter='\t')
             reader.next()
@@ -23,8 +31,8 @@ class MagHarvester2:
                     logger.info('{}:{}'.format(type(e).__name__, str(e)))
 
     def field_of_study_hierarchy(self, row):
-        FieldOfStudyHierarchy.objects.get_or_create(child_id=row[0],
-                                                    child_level=row[1],
-                                                    parent_id=row[2],
-                                                    parent_level=row[3],
-                                                    confidence=row[4])
+        FieldOfStudyHierarchy.objects.using(self.database).get_or_create(child_id=row[0],
+                                                                         child_level=row[1],
+                                                                         parent_id=row[2],
+                                                                         parent_level=row[3],
+                                                                         confidence=row[4])
