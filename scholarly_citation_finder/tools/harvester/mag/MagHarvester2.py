f4a460763ecebb3be724a8a42c8006891e368ef2
Xennis <code@xennis.org>
2016-03-27 15:29:17 +0200

    update new mag harvester

--- scholarly_citation_finder/tools/harvester/mag/MagHarvester2.py
+++ scholarly_citation_finder/tools/harvester/mag/MagHarvester2.py
@@ -8,6 +8,7 @@ from scholarly_citation_finder.apps.core.models import FieldOfStudyHierarchy,\
 	PublicationKeyword, PublicationFieldOfStudy, FieldOfStudy
 from .MagNormalize import MagNormalize, get_pre_name
 from scholarly_citation_finder.lib.django import queryset_iterator
+#from django.db import connections
 
 logger = logging.getLogger(__name__)
 
@@ -17,6 +18,9 @@ class MagHarvester2:
 	def __init__(self, path, database='mag'):
 		self.path = path
 		self.database = database
+		
+		#self.conn = connections[database]
+		#self.cursor = self.conn.cursor()
 
 	def run(self, name, callback):
 		if name not in MagNormalize.FILES:
@@ -37,37 +41,41 @@ class MagHarvester2:
 		for keyword in queryset_iterator(query):
 			result = self.__store_fieldofstudie_hierarchy(keyword.publication_id, fieldofstudy_id=keyword.fieldofstudy_id)
 			if not result:
-				self.__store_fieldofstudy(keyword.publication_id, fieldofstudy_id=keyword.fieldofstudy_id, confidence=1)
+				try:
+					fieldofstudy = FieldOfStudy.objects.using(self.database).get(id=keyword.fieldofstudy_id)
+					self.__store_fieldofstudy(keyword.publication_id, fieldofstudy_id=fieldofstudy.id, name=fieldofstudy.name, confidence=1)
+				except(ObjectDoesNotExist) as e:
+					logger.info(str(e))
 					
-	def __store_fieldofstudy(self, publication_id, fieldofstudy_id=None, name=None, level=10, confidence=1):
+	def __store_fieldofstudy(self, publication_id, fieldofstudy_id, name=None, level=10, confidence=1):
 		try:
-			if not name:
-				fieldofstudy = FieldOfStudy.objects.using(self.database).get(id=fieldofstudy_id)
-				name = fieldofstudy.name
 			PublicationFieldOfStudy.objects.using(self.database).create(publication_id=publication_id,
-																		name=name,
+																		fieldofstudy_id=fieldofstudy_id,
+																		fieldofstudy_name=name,
 																		level=level,
 																		confidence=confidence)
+			return True
 		except(IntegrityError) as e:
-			pass
-		except(ObjectDoesNotExist) as e:
 			logger.info(str(e))
+		return False
 
-	def __store_fieldofstudie_hierarchy(self, publication_id, fieldofstudy_id, store_child=True, recursive=False):
-		result = False
+	def __store_fieldofstudie_hierarchy(self, publication_id, fieldofstudy_id, recursive=False):
+		result = []
 
 		query = FieldOfStudyHierarchy.objects.using(self.database).filter(child_id=fieldofstudy_id)
 		for hierarchy in query.iterator():
 			result = True
 			# store child (level 3 to 1)
-			if store_child:
-				self.__store_fieldofstudy(publication_id, name=hierarchy.child.name, level=hierarchy.child_level, confidence=1)
-				store_child=False
+			if hierarchy.child_id not in result:
+				if self.__store_fieldofstudy(publication_id, fieldofstudy_id=hierarchy.child_id, name=hierarchy.child.name, level=hierarchy.child_level, confidence=1):
+					result.append(hierarchy.child_id)
 
 			# store parent (level 2 to 0)
-			self.__store_fieldofstudy(publication_id, name=hierarchy.parent.name, level=hierarchy.parent_level, confidence=hierarchy.confidence)				
+			if hierarchy.parent_id not in result:
+				if self.__store_fieldofstudy(publication_id, fieldofstudy_id=hierarchy.parent_id, name=hierarchy.parent.name, level=hierarchy.parent_level, confidence=hierarchy.confidence):
+					result.append(hierarchy.parent_id)
 
 			if recursive:
-				self.__lop(publication_id, fieldofstudy_id=hierarchy.parent_id, store_child=False)
+				self.__lop(publication_id, fieldofstudy_id=hierarchy.parent_id, recursive=recursive)
 
 		return result
