5e416471909abf5923c7166c189a820fcc6432c6
Xennis <code@xennis.org>
2016-04-18 14:17:22 +0200

    make min_year optional

--- scholarly_citation_finder/apps/citation/strategy/FieldofstudyStrategy.py
+++ scholarly_citation_finder/apps/citation/strategy/FieldofstudyStrategy.py
@@ -9,15 +9,17 @@ logger = logging.getLogger(__name__)
 
 class FieldofstudyStrategy(Strategy):
 
-
-    def __init__(self, ordered=False, limit=None):
+    def __init__(self, ordered=False, min_year=False, limit=None):
         name = 'fieldofstudy'
         if ordered:
             name += '-ordered'
+        if min_year:
+            name += '-minyear'
         if limit > 0:
             name += '-limit'
         super(FieldofstudyStrategy, self).__init__(name=name)
         self.ordered = ordered
+        self.min_year = min_year
         self.limit = limit
         
     def run(self, publication_set, callback):
@@ -25,16 +27,21 @@ class FieldofstudyStrategy(Strategy):
         if self.limit > 0:
             fieldofstudies = fieldofstudies[:self.limit]
         logger.info('found {} (limit: {}) field of studies in search set'.format(len(fieldofstudies), self.limit))
-        fieldofstudies_publications = self.__find_fieldofstudies_publications(fieldofstudies, publication_set.get_min_year())
-        callback(fieldofstudies_publications, 'field of studies (limit={})'.format(self.limit))
+
+        min_year = publication_set.get_min_year() if self.min_year else None
+        fieldofstudies_publications = self.__find_fieldofstudies_publications(fieldofstudies, min_year=min_year)
+        callback(fieldofstudies_publications, 'field of studies (year>={}, limit={})'.format(min_year, self.limit))
         #for fieldofstudy in fieldofstudies:
         #    fieldofstudy_publications = self.__find_fieldofstudy_publications(fieldofstudy.id, publication_set.get_min_year())
         #    fieldofstudy_publications_citing = citing_papers.filter(publication__in=fieldofstudy_publications)
         #    logger.info('field of study "{}": found {} publications (year >= {}), {} citations'.format(fieldofstudy, len(fieldofstudy_publications), publication_set.get_min_year(), len(fieldofstudy_publications_citing)))  
         #    citation_set.add(fieldofstudy_publications_citing, len(fieldofstudy_publications))
 
-    def __find_fieldofstudies_publications(self, fieldofstudies, min_year):
-        return Publication.objects.using(self.database).filter(publicationkeyword__fieldofstudy__in=fieldofstudies).filter(year__gte=min_year).distinct()
+    def __find_fieldofstudies_publications(self, fieldofstudies, min_year=None):
+        query = Publication.objects.using(self.database).filter(publicationkeyword__fieldofstudy__in=fieldofstudies)
+        if min_year:
+            query = query.filter(year__gte=min_year)
+        return query.distinct()
 
         
     #def __find_fieldofstudy_publications(self, fieldofstudy_id, min_year):
